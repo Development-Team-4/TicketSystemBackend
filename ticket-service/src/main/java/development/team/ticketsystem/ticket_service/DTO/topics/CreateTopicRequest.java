b50ea0871824336b9d5ada4c04213eea5fc5f300
IvanDrv <ivander05@mail.ru>
2026-03-15 17:47:18 +0300

    Data transfer objects for topic operations

---
+++ ticket-service/src/main/java/development/team/ticketsystem/ticket_service/DTO/topics/CreateTopicRequest.java
@@ -0,0 +1,15 @@
+package development.team.ticketsystem.ticket_service.DTO.topics;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class CreateTopicRequest {
+    private String name;
+    private String description;
+}
