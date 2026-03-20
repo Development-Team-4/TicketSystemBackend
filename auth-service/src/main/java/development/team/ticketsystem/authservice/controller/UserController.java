2d3792ad16350595c5e6c70f659034a3af1d42dd
D-ASA-D <vladruadov@gmail.com>
2026-03-21 02:54:12 +0300

    updating controllers with a new service layer and logging

--- auth-service/src/main/java/development/team/ticketsystem/authservice/controller/UserController.java
+++ auth-service/src/main/java/development/team/ticketsystem/authservice/controller/UserController.java
@@ -4,34 +4,40 @@ import development.team.ticketsystem.authservice.dto.notification.NotificationSe
 import development.team.ticketsystem.authservice.dto.notification.UpdateNotificationSettingsRequest;
 import development.team.ticketsystem.authservice.dto.user.UpdateUserRequest;
 import development.team.ticketsystem.authservice.dto.user.UserResponse;
+import development.team.ticketsystem.authservice.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 @Tag(name = "Users", description = "Пользователи")
 public class UserController {
 
+    private final UserService userService;
+
     @Operation(summary = "Получить пользователя по ID")
     @GetMapping("/{id}")
     public UserResponse getUser(
             @Parameter(description = "ID пользователя", example = "550e8400-e29b-41d4-a716-446655440000")
             @PathVariable UUID id) {
-        return new UserResponse();
+        log.info("Request has been sent to retrieve a user with ID {}", id);
+        return userService.getById(id);
     }
 
     @Operation(summary = "Получить список пользователей")
     @GetMapping
     public List<UserResponse> getUsers() {
-        return new ArrayList<>();
+        log.info("Request has been sent to retrieve all users");
+        return userService.getAll();
     }
 
     @Operation(summary = "Обновить пользователя")
@@ -40,8 +46,8 @@ public class UserController {
             @PathVariable UUID id,
             @RequestBody UpdateUserRequest request
     ) {
-
-        return new UserResponse();
+        log.info("Request has been sent to update the user's data with ID {}", id);
+        return userService.update(id, request);
     }
 
     @Operation(summary = "Обновить настройки уведмолений пользователя")
@@ -51,7 +57,8 @@ public class UserController {
             @PathVariable UUID id,
             @RequestBody UpdateNotificationSettingsRequest request
     ) {
-        return new NotificationSettingsResponse();
+        log.info("Request has been sent to update the notification settings for user with ID {}", id);
+        return userService.updateSettings(id, request);
     }
 
     @Operation(summary = "Получить настройки уведмолений пользователя")
@@ -59,7 +66,8 @@ public class UserController {
     public NotificationSettingsResponse getSettings(
             @PathVariable UUID id
     ) {
-        return new NotificationSettingsResponse();
+        log.info("Request has been sent to receive notification settings for user with id {}", id);
+        return userService.getSettings(id);
     }
 
 }
