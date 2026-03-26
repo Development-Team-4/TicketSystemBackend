88d529ccbc8123a45f5795fe938478da46789042
IvanDrv <ivander05@mail.ru>
2026-03-26 20:15:24 +0300

    Feat (Microservices' connection): Notification sending method improved

--- ticket-service/src/main/java/development/team/ticketsystem/ticketservice/Service/NotificationSender.java
+++ ticket-service/src/main/java/development/team/ticketsystem/ticketservice/Service/NotificationSender.java
@@ -1,28 +1,47 @@
 package development.team.ticketsystem.ticketservice.Service;
 
+import development.team.ticketsystem.ticketservice.Exceptions.NotificationServiceException;
 import development.team.ticketsystem.ticketservice.ForNotificationMicroservice.dto.NotificationCreationDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
 
 import java.util.UUID;
 
-
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class NotificationSender {
-    private final RestClient restClient = RestClient.create();
-    private static final String BASE_URL = "http://localhost:8083/notifications/";
+
+    private final RestClient restClient;
+
+    @Value("${services.notification.url:http://localhost:8083}")
+    private String notificationServiceUrl;
 
     public void sendToNotificationMicroservice(
             UUID toUserId,
             NotificationCreationDto request
     ) {
-        restClient.post()
-                .uri(BASE_URL + toUserId.toString())
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(request);
+        String url = notificationServiceUrl + "/notifications/" + toUserId;
+
+        try {
+            restClient.post()
+                    .uri(url)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(request)
+                    .retrieve()
+                    .toBodilessEntity();
+
+        } catch (RestClientException e) {
+            throw new NotificationServiceException(
+                    String.format("Failed to send notification to user %s", toUserId), e
+            );
+        }
     }
 }
 
