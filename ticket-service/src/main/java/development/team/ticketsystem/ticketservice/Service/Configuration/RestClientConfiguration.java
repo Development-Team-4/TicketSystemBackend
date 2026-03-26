53ee606eb2bc80dd9a4985f2f4f03f63e8fd2f86
IvanDrv <ivander05@mail.ru>
2026-03-26 20:12:07 +0300

    Feat (Notification sending): RestClient configuration

---
+++ ticket-service/src/main/java/development/team/ticketsystem/ticketservice/Service/Configuration/RestClientConfiguration.java
@@ -0,0 +1,28 @@
+package development.team.ticketsystem.ticketservice.Service.Configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfiguration {
+
+    @Value("${services.notification.timeout.connect:500}")
+    private int connectTimeout;
+
+    @Value("${services.notification.timeout.read:1000}")
+    private int readTimeout;
+
+    @Bean
+    public RestClient restClient() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(connectTimeout);
+        factory.setReadTimeout(readTimeout);
+
+        return RestClient.builder()
+                .requestFactory(factory)
+                .build();
+    }
+}
