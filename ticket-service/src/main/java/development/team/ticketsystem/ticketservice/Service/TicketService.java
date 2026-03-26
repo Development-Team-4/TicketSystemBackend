d62d5ca2e9f164af2a78c28f31b996a8e64e0846
IvanDrv <ivander05@mail.ru>
2026-03-26 15:16:44 +0300

    Feat (business logic): Implemented checking of basic business rules

--- ticket-service/src/main/java/development/team/ticketsystem/ticketservice/Service/TicketService.java
+++ ticket-service/src/main/java/development/team/ticketsystem/ticketservice/Service/TicketService.java
@@ -2,17 +2,22 @@ package development.team.ticketsystem.ticketservice.Service;
 
 import development.team.ticketsystem.ticketservice.DTO.tickets.*;
 import development.team.ticketsystem.ticketservice.Entity.TicketEntity;
+import development.team.ticketsystem.ticketservice.Exceptions.InvalidStateException;
 import development.team.ticketsystem.ticketservice.Repository.Specification.TicketSpecification;
 import development.team.ticketsystem.ticketservice.Repository.TicketRepository;
 import development.team.ticketsystem.ticketservice.TicketStatus;
+import development.team.ticketsystem.ticketservice.UserRole;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
+import development.team.ticketsystem.ticketservice.Exceptions.AccessDeniedException;
+import development.team.ticketsystem.ticketservice.TicketStatus;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -20,14 +25,23 @@ import java.util.UUID;
 public class TicketService {
 
     private final TicketRepository repository;
+    private final CategoryStaffService categoryStaffService;
 
-    public TicketResponse create(CreateTicketRequest request) {
+    private static final Map<TicketStatus, Set<TicketStatus>> ALLOWED_TRANSITIONS = Map.of(
+            TicketStatus.OPEN, Set.of(TicketStatus.ASSIGNED, TicketStatus.CLOSED),
+            TicketStatus.ASSIGNED, Set.of(TicketStatus.IN_PROGRESS, TicketStatus.CLOSED),
+            TicketStatus.IN_PROGRESS, Set.of(TicketStatus.RESOLVED, TicketStatus.ASSIGNED),
+            TicketStatus.RESOLVED, Set.of(TicketStatus.CLOSED, TicketStatus.ASSIGNED),
+            TicketStatus.CLOSED, Set.of()
+    );
+
+    public TicketResponse create(UUID authorId, CreateTicketRequest request) {
         TicketEntity ticket = TicketEntity.builder()
                 .subject(request.getSubject())
                 .description(request.getDescription())
                 .categoryId(request.getCategoryId())
                 .status(TicketStatus.OPEN)
-                .createdBy(UUID.fromString("3e3789e6-74ab-4101-95f2-f88876b0ed8c")) // временно
+                .createdBy(authorId)
                 .createdAt(Instant.now())
                 .updatedAt(Instant.now())
                 .build();
@@ -37,6 +51,8 @@ public class TicketService {
     }
 
     public List<TicketResponse> getAll(
+            UserRole role,
+            UUID userId,
             UUID categoryId,
             UUID assignedTo,
             UUID createdBy,
@@ -44,6 +60,29 @@ public class TicketService {
             Instant createdAfter,
             Instant createdBefore
     ) {
+
+        if (role.equals(UserRole.SUPPORT)) {
+            if (categoryId == null) {
+                categoryId = categoryStaffService.getByUser(userId).getFirst().getCategoryId();
+            }
+            if (assignedTo == null) {
+                assignedTo = userId;
+            } else if (!assignedTo.equals(userId)) {
+                throw new AccessDeniedException("Staff can not view tickets assigned to others");
+            }
+
+            if (createdBy != null) {
+                throw new AccessDeniedException("Unable to filter by createdBy");
+            }
+        } else if (role.equals(UserRole.USER)) {
+            createdBy = userId;
+
+            if (assignedTo != null) {
+                throw new AccessDeniedException("Users can not filter by assignedTo");
+            }
+        }
+
+
         return repository.findAll(
                         TicketSpecification.filter(
                                 categoryId,
@@ -64,9 +103,18 @@ public class TicketService {
     }
 
     @Transactional
-    public TicketResponse update(UUID id, UpdateTicketRequest request) {
+    public TicketResponse update(
+            UserRole role,
+            UUID userId,
+            UUID id,
+            UpdateTicketRequest request) {
         TicketEntity existing = repository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Ticket not found"));
+        checkNotClosed(existing);
+
+        if (role == UserRole.USER && !existing.getCreatedBy().equals(userId)) {
+            throw new AccessDeniedException("Cannot edit other user's ticket");
+        }
 
         existing.setSubject(request.getSubject())
                 .setDescription(request.getDescription())
@@ -77,10 +125,17 @@ public class TicketService {
     }
 
     @Transactional
-    public void delete(UUID id) {
-        if (!repository.existsById(id)) {
-            throw new EntityNotFoundException("Ticket not found");
+    public void delete(UserRole role, UUID userId, UUID id) {
+        TicketEntity ticket = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Ticket not found"));
+
+        if (ticket.getStatus() != TicketStatus.OPEN) {
+            throw new InvalidStateException("Only OPEN tickets can be deleted");
         }
+        if (role == UserRole.USER && !ticket.getCreatedBy().equals(userId)) {
+            throw new InvalidStateException("You can not delete ticket of other user");
+        }
+
         repository.deleteById(id);
     }
 
@@ -88,6 +143,16 @@ public class TicketService {
     public TicketResponse updateStatus(UUID id, UpdateStatusRequest request) {
         TicketEntity ticket = repository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Ticket not found"));
+        checkNotClosed(ticket);
+
+        TicketStatus current = ticket.getStatus();
+
+        if (!ALLOWED_TRANSITIONS.get(current).contains(request.getStatus())) {
+            throw new InvalidStateException(
+                    "Invalid status transition: " + current + " -> " + request.getStatus()
+            );
+        }
+
         ticket.setStatus(request.getStatus())
                 .setUpdatedAt(Instant.now());
         TicketEntity updated = repository.save(ticket);
@@ -98,11 +163,22 @@ public class TicketService {
     public TicketResponse assign(UUID id, AssignTicketRequest assigneeId) {
         TicketEntity ticket = repository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Ticket not found"));
+        checkNotClosed(ticket);
+
+        if (ticket.getStatus().equals(TicketStatus.CLOSED)) {
+            throw new InvalidStateException("Cannot assign closed ticket");
+        }
 
         ticket.setAssigneeId(assigneeId.getAssigneeId())
                 .setUpdatedAt(Instant.now());
         TicketEntity assigned = repository.save(ticket);
         return toResponse(assigned);
+    }
+
+    private void checkNotClosed(TicketEntity ticket) {
+        if (ticket.getStatus() == TicketStatus.CLOSED) {
+            throw new InvalidStateException("Ticket is CLOSED");
+        }
     }
 
     // mapper - ИСПОЛЬЗОВАТЬ БИБЛИОТЕКУ mapstruct
